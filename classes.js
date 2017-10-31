58bc7c0c43c44830477be09ef38abea939b1c07d
Neous1 <Neous1@users.noreply.github.com>
2017-10-31 10:38:24 -0400

    Added class constructor

--- classes.js
+++ classes.js
@@ -1,6 +1,24 @@
-function Building(floors){
-    this.what = "building";
-    this.floors = floors;
+// function Building(floors){
+//     this.what = "building";
+//     this.floors = floors;
+// }
+
+// Building.prototype.countFloors = function(){
+//     console.log(" i have ", this.floors, "floors");
+// }
+
+// var yourHouse = new Building(2);
+// var theOffice = new Building(12);
+
+// yourHouse.countFloors();
+// theOffice.countFloors();
+
+
+function FloorPlan(rooms){
+    this.windows = 3;
+    this.walls = 2;
 }
 
-Building.proto
+FloorPlan.prototype.build = function(){
+    
+}
